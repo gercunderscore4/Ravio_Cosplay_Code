2152270cc095ac964b729fb75a4269335e24f373
Geoffrey Card <gercunderscore4@gmail.com>
2018-11-29 05:07:44 +0000

    working on multicolour code for LEDs, LED turns on, but have not tried colors yet

--- prototype_rod/main.c
+++ prototype_rod/main.c
@@ -19,13 +19,13 @@
  * PB3 -+O   +- PB2/SCL
  * PB4 -+    +- PB1/OC0B/OC1A
  * GND -+----+- PB0/SDA
- * 
+ *
  * PB0 : I2C (SDA)
  * PB1 : PWM (audio)
  * PB2 : I2c (SCL)
  * PB3 : LEDs (CO)
- * PB4 : NC
- * PB5 : LEDs (DO)
+ * PB4 : LEDs (DO)
+ * PB5 : NC
  */
 
 #include <avr/io.h>
@@ -42,10 +42,24 @@
 
 int main (void)
 {
-    rgb_color leds[LED_COUNT] = {{0xFF, 0xFF, 0xFF}};
+    rgb_color leds[LED_COUNT] = {{0xFF, 0x00, 0x00}};
+    uint8_t state = 0;
 
-    APA102WriteColors(leds, LED_COUNT);
     while (1) {
+        APA102WriteColors(leds, LED_COUNT);
+        if        (0 == state) {
+            if (0xFF == (++leds[0].green)) state = 1;
+        } else if (1 == state) {
+            if (0x00 == (--leds[0].red))   state = 2;
+        } else if (2 == state) {
+            if (0xFF == (++leds[0].blue))  state = 3;
+        } else if (3 == state) {
+            if (0x00 == (--leds[0].green)) state = 4;
+        } else if (4 == state) {
+            if (0xFF == (++leds[0].red))   state = 5;
+        } else if (5 == state) {
+            if (0x00 == (--leds[0].blue))  state = 0;
+        }
     }
     return 1;
 }
