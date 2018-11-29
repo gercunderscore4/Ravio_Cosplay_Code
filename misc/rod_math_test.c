bb1bb10df896d5528efbf18cebb10adfb5a0c651
Geoffrey Card <gcard@aversan.com>
2018-11-29 15:13:31 -0500

    fixed and optimized some accelerometer math

--- misc/rod_math_test.c
+++ misc/rod_math_test.c
@@ -6,21 +6,18 @@
 #include "stdint.h"
 #include "time.h"
 
-#define LED_COUNT   1
-#define BRIGHTNESS 31
-
 /*
  * estimate sqrt based on leading digit position
- * need to deal with values up to 3*0xFF*FF = 195075 = 0x2FA03
+ * need to deal with values up to 3*0x80*80 = 49152 = 0xC000
  * 
  * python code to generate best guess:
  * from math import log2, ceil, sqrt
  * d = [(0,0,0)]
- * d.extend([(i + 1, 1 << i, int(round(sqrt(1.5 * (1 << i))))) for i in range(ceil(log2(3 * 0xFF * 0xFF)) + 1)])
+ * d.extend([(i + 1, 1 << i, int(round(sqrt(1.5 * (1 << i))))) for i in range(ceil(log2(3 * 0x80 * 0x80)))])
  * for a,b,c in d:
  *     print('{:2d} {:12d} {:6d}'.format(a,b,c))
  */
-const uint16_t SQRT_BY_DIGIT[] = {  0,  //  0, sqrt(     0 * 1.5) // value in the middle of the range
+const uint16_t SQRT_BY_DIGIT[] = {  0,  //  0, sqrt(     0 * 1.5)
                                     1,  //  1, sqrt(     1 * 1.5)
                                     2,  //  2, sqrt(     2 * 1.5)
                                     2,  //  3, sqrt(     4 * 1.5)
@@ -36,32 +33,43 @@ const uint16_t SQRT_BY_DIGIT[] = {  0,  //  0, sqrt(     0 * 1.5) // value in th
                                    78,  // 13, sqrt(  4096 * 1.5)
                                   111,  // 14, sqrt(  8192 * 1.5)
                                   157,  // 15, sqrt( 16384 * 1.5)
-                                  222,  // 16, sqrt( 32768 * 1.5)
-                                  314,  // 17, sqrt( 65536 * 1.5)
-                                  443,  // 18, sqrt(131072 * 1.5)
-                                  627}; // 19, sqrt(262144 * 1.5)
+                                  222}; // 16, sqrt( 32768 * 1.5)
 
-int16_t maxOfThree(int16_t a, int16_t b, int16_t c)
+void processAccelData (int16_t x, int16_t y, int16_t z, uint8_t* r, uint8_t* g, uint8_t* b, uint16_t* f)
 {
-    if (a > b)
+    // max possibile are 0x7FFF and -0x8000
+    // shrink to one byte, take absolute
+    x = abs(x / 0x100);
+    y = abs(y / 0x100);
+    z = abs(z / 0x100);
+    
+    // max is 3 * 0x80 * 0x80 = 0xC000
+    uint32_t t = ((uint16_t) (x * x) + (y * y) + (z * z));
+    // get leading digit
+    uint8_t d = 0;
+    while (t > 0)
     {
-        if (a > c)
-        {
-            return a;
-        }
-        else 
-        {
-            return c;
-        }
+        t >>= 1;
+        d++;
     }
-    else
-    {
-        if (b > c)
-        {
-            return b;
-        }
+    // get estimated sqrt by digit
+    *f = SQRT_BY_DIGIT[(int)d];
+
+    // normalize so that largest is 255
+    // get max
+    uint8_t m = (x > y) ? ((x > z) ? x : z) : ((y > z) ? y : z);
+    // x * 0xFF / m
+    // x * (0x100 - 1) / m
+    // ((x << 8) - x) / m
+    if (m == 0) {
+        *r = 0x00;
+        *g = 0x00;
+        *b = 0x00;
+    } else {
+        *r = (uint8_t) (((((uint16_t)x) << 8) - x) / m);
+        *g = (uint8_t) (((((uint16_t)y) << 8) - y) / m);
+        *b = (uint8_t) (((((uint16_t)z) << 8) - z) / m);
     }
-    return c;
 }
 
 int main (void)
@@ -75,50 +83,62 @@ int main (void)
     int16_t x = 0; // x-axis acceleration
     int16_t y = 0; // y-axis acceleration
     int16_t z = 0; // z-axis acceleration
+   
+    printf("Hello, brave, new World!\n");
     
-    int16_t tx = 0; // temp
-    int16_t ty = 0; 
-    int16_t tz = 0; 
-    
-    uint16_t m = 0; // max of new x,y,z
-    uint32_t t = 0;
-    uint8_t d = 0;
-    
-    srand(time(NULL));
+    // largest negative
+    x = -32768;
+    y = -32768;
+    z = -32768;
+    processAccelData (x, y, z, &r, &g, &b, &f);
+    printf("(X,Y,Z) -> (%5hX %5hX %5hX)    RGB: %02X %02X %02X    f: %5d\n", x, y, z, r, g, b, f);
 
+    // largest positive
+    x = 0x7FFF;
+    y = 0x7FFF;
+    z = 0x7FFF;
+    processAccelData (x, y, z, &r, &g, &b, &f);
+    printf("(X,Y,Z) -> (%5hX %5hX %5hX)    RGB: %02X %02X %02X    f: %5d\n", x, y, z, r, g, b, f);
+
+    // all zero
+    x = 0x0;
+    y = 0x0;
+    z = 0x0;
+    processAccelData (x, y, z, &r, &g, &b, &f);
+    printf("(X,Y,Z) -> (%5hX %5hX %5hX)    RGB: %02X %02X %02X    f: %5d\n", x, y, z, r, g, b, f);
+
+    // 1,0,0 after reduction
+    x = 0x100;
+    y = 0x0;
+    z = 0x0;
+    processAccelData (x, y, z, &r, &g, &b, &f);
+    printf("(X,Y,Z) -> (%5hX %5hX %5hX)    RGB: %02X %02X %02X    f: %5d\n", x, y, z, r, g, b, f);
+
+    // eh, should be easy
+    x = 0xFF0;
+    y = 0x800;
+    z = 0x0;
+    processAccelData (x, y, z, &r, &g, &b, &f);
+    printf("(X,Y,Z) -> (%5hX %5hX %5hX)    RGB: %02X %02X %02X    f: %5d\n", x, y, z, r, g, b, f);
+
+    // negative numbers
+    x =  0xFF0;
+    y = -0xFF0;
+    z = -0xFF0;
+    processAccelData (x, y, z, &r, &g, &b, &f);
+    printf("(X,Y,Z) -> (%5hX %5hX %5hX)    RGB: %02X %02X %02X    f: %5d\n", x, y, z, r, g, b, f);
+
+    srand(time(NULL));
     for (int i = 0; i < 10; i++)
     {
         // get "data"
-        x = rand() % 0xFFFF; // RAND_MAX is guaranteed to be at least 32767
+        x = rand() % 0xFFFF;
         y = rand() % 0xFFFF;
         z = rand() % 0xFFFF;
-        
-        // shrink to one byte, abs, ensure smaller than 256 (negative max)
-        tx = abs(x / 0x100);
-        ty = abs(y / 0x100);
-        tz = abs(z / 0x100);
-        tx = (tx >= 0x100) ? 0xFF : tx;
-        ty = (ty >= 0x100) ? 0xFF : ty;
-        tz = (tz >= 0x100) ? 0xFF : tz;
-        
-        // normalize so that largest is 255
-        m = (int16_t) maxOfThree(tx, ty, tz);
-        r = (uint8_t) (((uint16_t) tx) * 0xFF / m);
-        g = (uint8_t) (((uint16_t) ty) * 0xFF / m);
-        b = (uint8_t) (((uint16_t) tz) * 0xFF / m);
-        
-        t = ((uint32_t) (tx*tx) + (ty*ty) + (tz*tz));
-        d = 0;
-        while (t > 0)
-        {
-            t >>= 1;
-            d++;
-        }
-        t = ((uint32_t) (tx*tx) + (ty*ty) + (tz*tz));
-        f = SQRT_BY_DIGIT[(int)d];
-
-        printf("RGB: %02X %02X %02X    f: %5d    (X,Y,Z) -> (%5X %5X %5X)    d = %2d    t = %6d\n", r, g, b, f, x, y, z, d, t);
+        processAccelData (x, y, z, &r, &r, &r, &f);
+        printf("(X,Y,Z) -> (%5hX %5hX %5hX)    RGB: %02X %02X %02X    f: %5d\n", x, y, z, r, g, b, f);
     }
     
+    printf("Good-bye, cruel World!\n");
     return 0;
 }
