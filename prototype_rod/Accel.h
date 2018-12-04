19462835ee0def55c79acc3897eaeeb2016c6420
Geoffrey Card <gercunderscore4@gmail.com>
2018-12-03 21:24:03 -0500

    adding files from a TWI library

--- prototype_rod/Accel.h
+++ prototype_rod/Accel.h
@@ -5,8 +5,12 @@
 #ifndef __ACCEL_H__
 #define __ACCEL_H__
 
-#include "I2C.h"
+#include "USI_TWI_Master.h"
 
 /* ... */
+//void USI_TWI_Master_Initialise(void);
+//unsigned char USI_TWI_Start_Transceiver_With_Data_Stop(unsigned char *, unsigned char, unsigned char);
+//unsigned char USI_TWI_Start_Transceiver_With_Data(unsigned char *, unsigned char);
+//unsigned char USI_TWI_Get_State_Info(void);
 
 #endif // __ACCEL_H__
