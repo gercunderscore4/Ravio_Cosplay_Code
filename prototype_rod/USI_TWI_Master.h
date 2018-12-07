96080aca028d57e44af4fa66eca3b09f5562bea2
Geoffrey Card <gercunderscore4@gmail.com>
2018-12-06 19:22:25 -0500

    grabbing updated TWI library

--- prototype_rod/USI_TWI_Master.h
+++ prototype_rod/USI_TWI_Master.h
@@ -1,89 +1,143 @@
 /*****************************************************************************
 *
-* Atmel Corporation
 *
-* File              : USI_TWI_Master.h
-* Date              : $Date: 2016-7-15 $
-* Updated by        : $Author: Atmel $
+* File              USI_TWI_Master.h compiled with gcc
+* Date              Friday, 10/31/08		Boo!
+* Updated by        jkl
 *
-* Support mail      : avr@atmel.com
 *
 * Supported devices : All device with USI module can be used.
-*                     The example is written for the ATmega169, ATtiny26 and ATtiny2313
+*                     The example is written for the ATtiny2313
 *
 * AppNote           : AVR310 - Using the USI module as a TWI Master
 *
-* Description       : This is an implementation of an TWI master using
-*                     the USI module as basis. The implementation assumes the AVR to
-*                     be the only TWI master in the system and can therefore not be
-*                     used in a multi-master system.
-* Usage             : Initialize the USI module by calling the USI_TWI_Master_Initialise()
-*                     function. Hence messages/data are transceived on the bus using
-*                     the USI_TWI_Start_Transceiver_With_Data() function. If the transceiver
-*                     returns with a fail, then use USI_TWI_Get_Status_Info to evaluate the
-*                     couse of the failure.
+* This is modified to just do I2C communication on ATtiny2313 running at 
+*	1MHz. Fast mode is probably possible, but would need a faster clock
+*	and has not been tested.
 *
+*	12/15/08	Added declaration of USI_TWI_Start_Memory_Read	-jkl
 ****************************************************************************/
-#include <avr/io.h>
-#include <util/delay.h>
+    
 //********** Defines **********//
-// Defines controlling timing limits
-#define TWI_FAST_MODE
 
-#define SYS_CLK 4000.0 // [kHz]
+// Defines controlling timing limits - SCL <= 100KHz.
 
-#ifdef TWI_FAST_MODE                            // TWI FAST mode timing limits. SCL = 100-400kHz
-#define T2_TWI ((SYS_CLK * 1300) / 1000000) + 1 // >1,3us
-#define T4_TWI ((SYS_CLK * 600) / 1000000) + 1  // >0,6us
+// For use with _delay_us()
+#define T2_TWI    5	    	// >4,7us:  
+#define T4_TWI    4 		// >4,0us:  
 
-#else                                           // TWI STANDARD mode timing limits. SCL <= 100kHz
-#define T2_TWI ((SYS_CLK * 4700) / 1000000) + 1 // >4,7us
-#define T4_TWI ((SYS_CLK * 4000) / 1000000) + 1 // >4,0us
-#endif
-
-// Defines controling code generating
+// Defines error code generating
 //#define PARAM_VERIFICATION
 //#define NOISE_TESTING
-#define SIGNAL_VERIFY
+#define SIGNAL_VERIFY		// This should probably be on always.
 
-// USI_TWI messages and flags and bit masks
-//#define SUCCESS   7
-//#define MSG       0
 /****************************************************************************
   Bit and byte definitions
 ****************************************************************************/
-#define TWI_READ_BIT 0 // Bit position for R/W bit in "address byte".
-#define TWI_ADR_BITS 1 // Bit position for LSB of the slave address bits in the init byte.
-#define TWI_NACK_BIT 0 // Bit position for (N)ACK bit.
+#define TWI_READ_BIT  0       // Bit position for R/W bit in "address byte".
+#define TWI_ADR_BITS  1       // Bit position for LSB of the slave address bits in the init byte.
+#define TWI_NACK_BIT  0       // Bit position for (N)ACK bit.
 
-#define USI_TWI_NO_DATA 0x00           // Transmission buffer is empty
-#define USI_TWI_DATA_OUT_OF_BOUND 0x01 // Transmission buffer is outside SRAM space
-#define USI_TWI_UE_START_CON 0x02      // Unexpected Start Condition
-#define USI_TWI_UE_STOP_CON 0x03       // Unexpected Stop Condition
-#define USI_TWI_UE_DATA_COL 0x04       // Unexpected Data Collision (arbitration)
-#define USI_TWI_NO_ACK_ON_DATA 0x05    // The slave did not acknowledge  all data
-#define USI_TWI_NO_ACK_ON_ADDRESS 0x06 // The slave did not acknowledge  the address
-#define USI_TWI_MISSING_START_CON 0x07 // Generated Start Condition not detected on bus
-#define USI_TWI_MISSING_STOP_CON 0x08  // Generated Stop Condition not detected on bus
+// Note these have been renumbered from the Atmel Apps Note. Most likely errors are now
+//		lowest numbers so they're easily recognized as LED flashes.
+#define USI_TWI_NO_DATA             0x08  // Transmission buffer is empty
+#define USI_TWI_DATA_OUT_OF_BOUND   0x09  // Transmission buffer is outside SRAM space
+#define USI_TWI_UE_START_CON        0x07  // Unexpected Start Condition
+#define USI_TWI_UE_STOP_CON         0x06  // Unexpected Stop Condition
+#define USI_TWI_UE_DATA_COL         0x05  // Unexpected Data Collision (arbitration)
+#define USI_TWI_NO_ACK_ON_DATA      0x02  // The slave did not acknowledge  all data
+#define USI_TWI_NO_ACK_ON_ADDRESS   0x01  // The slave did not acknowledge  the address
+#define USI_TWI_MISSING_START_CON   0x03  // Generated Start Condition not detected on bus
+#define USI_TWI_MISSING_STOP_CON    0x04  // Generated Stop Condition not detected on bus
+#define USI_TWI_BAD_MEM_READ	    0x0A  // Error during external memory read
 
-// Device dependant defines
-#define DDR_USI DDRB
-#define PORT_USI PORTB
-#define PIN_USI PINB
-#define PORT_USI_SDA PORTB0
-#define PORT_USI_SCL PORTB2
-#define PIN_USI_SDA PINB0
-#define PIN_USI_SCL PINB2
+// Device dependant defines ADDED BACK IN FROM ORIGINAL ATMEL .H
+
+#if defined(__AVR_AT90Mega169__) | defined(__AVR_ATmega169__) | \
+    defined(__AVR_AT90Mega165__) | defined(__AVR_ATmega165__) | \
+    defined(__AVR_ATmega325__) | defined(__AVR_ATmega3250__) | \
+    defined(__AVR_ATmega645__) | defined(__AVR_ATmega6450__) | \
+    defined(__AVR_ATmega329__) | defined(__AVR_ATmega3290__) | \
+    defined(__AVR_ATmega649__) | defined(__AVR_ATmega6490__)
+    #define DDR_USI             DDRE
+    #define PORT_USI            PORTE
+    #define PIN_USI             PINE
+    #define PORT_USI_SDA        PORTE5
+    #define PORT_USI_SCL        PORTE4
+    #define PIN_USI_SDA         PINE5
+    #define PIN_USI_SCL         PINE4
+#endif
+
+#if defined(__AVR_ATtiny25__) | defined(__AVR_ATtiny45__) | defined(__AVR_ATtiny85__) | \
+    defined(__AVR_AT90Tiny26__) | defined(__AVR_ATtiny26__)
+    #define DDR_USI             DDRB
+    #define PORT_USI            PORTB
+    #define PIN_USI             PINB
+    #define PORT_USI_SDA        PORTB0
+    #define PORT_USI_SCL        PORTB2
+    #define PIN_USI_SDA         PINB0
+    #define PIN_USI_SCL         PINB2
+#endif
+
+#if defined(__AVR_ATtiny84__) | defined(__AVR_ATtiny44__)
+    #  define DDR_USI           DDRA
+    #  define PORT_USI          PORTA
+    #  define PIN_USI           PINA
+    #  define PORT_USI_SDA      PORTA6
+    #  define PORT_USI_SCL      PORTA4
+    #  define PIN_USI_SDA       PINA6
+    #  define PIN_USI_SCL       PINA4
+#endif
+
+#if defined(__AVR_ATtiny1634__)
+    #define DDR_USI             DDRB
+    #define PORT_USI            PORTB
+    #define PIN_USI             PINB
+    #define DDR_USI_CL             DDRC
+    #define PORT_USI_CL            PORTC
+    #define PIN_USI_CL             PINC
+    #define PORT_USI_SDA        PORTB1
+    #define PORT_USI_SCL        PORTC1
+    #define PIN_USI_SDA         PINB1
+    #define PIN_USI_SCL         PINC1
+#endif
+
+#if defined(__AVR_AT90Tiny2313__) | defined(__AVR_ATtiny2313__)
+    #define DDR_USI             DDRB
+    #define PORT_USI            PORTB
+    #define PIN_USI             PINB
+    #define PORT_USI_SDA        PORTB5
+    #define PORT_USI_SCL        PORTB7
+    #define PIN_USI_SDA         PINB5
+    #define PIN_USI_SCL         PINB7
+#endif
+
+/* From the original .h
+// Device dependant defines - These for ATtiny2313. // CHANGED FOR ATtiny85
+
+    #define DDR_USI             DDRB
+    #define PORT_USI            PORTB
+    #define PIN_USI             PINB
+    #define PORT_USI_SDA        PORTB0   // was PORTB5 - N/U
+    #define PORT_USI_SCL        PORTB2   // was PORTB7 - N/U
+    #define PIN_USI_SDA         PINB0    // was PINB5
+    #define PIN_USI_SCL         PINB2    // was PINB7
+*/
 
 // General defines
-#define TRUE 1
+#define TRUE  1
 #define FALSE 0
 
-#define DELAY_T2TWI (__delay_cycles(T2_TWI))
-#define DELAY_T4TWI (__delay_cycles(T4_TWI))
+#ifndef DDR_USI_CL
+    #define DDR_USI_CL DDR_USI
+    #define PORT_USI_CL PORT_USI
+#   define PIN_USI_CL PIN_USI
+#endif
+
 //********** Prototypes **********//
 
-void USI_TWI_Master_Initialise(void);
-unsigned char USI_TWI_Start_Transceiver_With_Data_Stop(unsigned char *, unsigned char, unsigned char);
-unsigned char USI_TWI_Start_Transceiver_With_Data(unsigned char *, unsigned char);
-unsigned char USI_TWI_Get_State_Info(void);
+void              USI_TWI_Master_Initialise( void );
+unsigned char USI_TWI_Start_Random_Read( unsigned char * , unsigned char );
+unsigned char USI_TWI_Start_Read_Write( unsigned char * , unsigned char );
+unsigned char USI_TWI_Master_Stop( void );
+unsigned char USI_TWI_Get_State_Info( void );
