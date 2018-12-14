76f7f5848ac68308f774805ac60401489001277b
Geoffrey Card <gcard@aversan.com>
2018-12-14 15:26:23 -0500

    adding accelerometer init

--- prototype_rod/Accel.c
+++ prototype_rod/Accel.c
@@ -74,7 +74,9 @@ void accelProcessData (int16_t x, int16_t y, int16_t z, uint8_t* r, uint8_t* g, 
 }
 
 void accelInit (void) {
+    uint8_t data[] = {0x30, 0xA0, 0x7F, 0x00, 0x00, 0x30, 0x00, 0x00};
     USI_TWI_Master_Initialise();
+    USI_TWI_Start_Transceiver_With_Data_Stop(data, sizeof(data), true);
 }
 
 void accelRead (int16_t* x, int16_t* y, int16_t* z) {
@@ -118,10 +120,17 @@ void accelRead (int16_t* x, int16_t* y, int16_t* z) {
      * SCL    \__|__|__|__|__|__|__|__|__|_______|__|__|__|__|__|__|__|__|____|__|__|__|__|__|__|__|__|__
      *
      */
-
-    USI_TWI_Master_Initialise();
+    
     USI_TWI_Start_Transceiver_With_Data_Stop(data1, sizeof(data1), false);
     USI_TWI_Start_Transceiver_With_Data_Stop(data2, sizeof(data2), true);
+
+    // read WHO_AM_I register
+    //data1[0] = 0x30;
+    //data1[1] = 0x8F;
+    //data2[0] = 0x31;
+    //data2[1] = 0x00;
+    //USI_TWI_Start_Transceiver_With_Data_Stop(data1, sizeof(data1), false);
+    //USI_TWI_Start_Transceiver_With_Data_Stop(data2, 2, true);
     
     //if (true == i2c_read(LIS2DH12_ADDR, LIS2DH12_OUT_X_L, data, 6)) {
 
