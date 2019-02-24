8231ceaad2ebac145a90704782c06dd0fd3c7453
Geoffrey Card <gercunderscore4@gmail.com>
2019-02-24 16:21:40 +0000

    all working, the version works, but I would like to try with a better sound chip

--- treasure_chest/main.c
+++ treasure_chest/main.c
@@ -23,9 +23,9 @@
  * PB5 : NC
  *
  * NC  -+----+- VCC
- * DO  -+O   +- NC
+ * DO  -+O   +- SW3
  * CO  -+    +- PWM
- * GND -+----+- NC
+ * GND -+----+- SW2
  */
 
 #include <avr/io.h>
@@ -38,20 +38,21 @@
 ////////////////////////////////////////////////////////////////
 
 #define LED_COUNT 65
-#define SPARKLE_COUNT ((LED_COUNT * 2) - 1)
+#define LEVEL_COUNT 8
+#define SPARKLE_COUNT (((LED_COUNT / LEVEL_COUNT) + 2) * LEVEL_COUNT)
 #define JIFFY_MS 25
 
 rgb_color sparkles[SPARKLE_COUNT];
 uint8_t sparkle_index = 0;
 
 void stop_sparkle (void) {
-    APA102WriteBlack(LED_COUNT);
+    APA102WriteBlack(SPARKLE_COUNT);
 }
 
 void sparkle (uint8_t jiffies) {
     for (uint8_t i = 0; i < jiffies; i++) {
         sparkle_index++;
-        sparkle_index %= LED_COUNT;
+        sparkle_index %= LEVEL_COUNT;
         APA102WriteColors(&(sparkles[sparkle_index]), LED_COUNT);
         _delay_ms(JIFFY_MS);
     }
@@ -62,13 +63,14 @@ void init_sparkle (void) {
 
     uint8_t temp = 0;
     for (uint8_t i = 0; i < SPARKLE_COUNT; i++) {
-        if ((i % 14) < 7) {
-            temp = (0x01 << (i % 7));
-        } else {
-            temp = (0x80 >> (i % 7));
+        temp = i % LEVEL_COUNT;
+        if ((i % (2 * LEVEL_COUNT)) < LEVEL_COUNT) {
+            temp = LEVEL_COUNT - temp;
         }
+        temp = 0xFF >> temp;
+
         sparkles[i].red   = temp;
-        sparkles[i].green = temp;
+        sparkles[i].green = temp - (temp >> 2);
         sparkles[i].blue  = 0x00;
     }
     stop_sparkle();
@@ -129,21 +131,31 @@ void play_section_4 (uint8_t* divindex, uint8_t* octave) {
     play_sparkle_tone((*divindex), (*octave), BEAT_IN_JIFFIES * 4);
 }
 
+void play_section_5 (uint8_t* divindex, uint8_t* octave) {
+    play_sparkle_tone((*divindex), (*octave), BEAT_IN_JIFFIES * 1);
+    inc_half(divindex, octave);
+    play_sparkle_tone((*divindex), (*octave), BEAT_IN_JIFFIES * 1);
+    inc_half(divindex, octave);
+    play_sparkle_tone((*divindex), (*octave), BEAT_IN_JIFFIES * 1);
+    inc_half(divindex, octave);
+    play_sparkle_tone((*divindex), (*octave), BEAT_IN_JIFFIES * 4);
+}
+
 ////////////////////////////////////////////////////////////////
 // SWITCHES
 ////////////////////////////////////////////////////////////////
 
 void init_switches (void) {
     DDRB  &=  ~( (1 << DDB0) | (1 << DDB2) ); // inputs
-    PORTB |=  (1 << PORTB0) | (1 << PORTB2);      // pull-up
+    PORTB |=  (1 << PORTB0) | (1 << PORTB2); // pull-up
 }
 
 uint8_t get_sw2 (void) {
-    return !(PINB & PINB0)
+    return !(PINB & ( 1 << PINB0));
 }
 
 uint8_t get_sw3 (void) {
-    return !(PINB & PINB2)
+    return !(PINB & (1 << PINB2));
 }
 
 ////////////////////////////////////////////////////////////////
@@ -152,26 +164,32 @@ uint8_t get_sw3 (void) {
 
 int main (void)
 {
+    init_switches();
     init_sparkle();
     init_audio();
 
-
-    uint8_t* divindex = 4;
-    uint8_t* octave   = 4;
+    uint8_t divindex = 4;
+    uint8_t octave   = 4;
     play_section_0(&divindex, &octave);
-    
-    played_2 = 0;
-    played_4 = 0;
+
+    uint8_t played_1 = 0;
+    uint8_t played_2 = 0;
+    uint8_t played_4 = 0;
     while (!played_4) {
         if (get_sw3()) {
-            if (played_2) play_section_3(&divindex, &octave);
-            play_section_4(&divindex, &octave);
+            if (played_2 || played_1) {
+                play_section_3(&divindex, &octave);
+                play_section_4(&divindex, &octave);
+            } else {
+                play_section_5(&divindex, &octave);
+            }
             played_4 = 1;
-        } else if (get_sw2()){
+        } else if (get_sw2() || played_2){
             play_section_2(&divindex, &octave);
             played_2 = 1;
         } else {
             play_section_1(&divindex, &octave);
+            played_1 = 1;
         }
     }
     stop_sparkle();
