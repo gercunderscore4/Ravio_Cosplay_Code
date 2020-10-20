74ef44d73457efd3374439550275b177b0becea4
Geoffrey Card <gercunderscore4@gmail.com>
2020-10-20 03:47:04 -0400

    started making plots for rack and pinion

--- hookshot/hookshot_v3.m
+++ hookshot/hookshot_v3.m
@@ -1,95 +1,468 @@
-% Coordinates from:
-%     http://hessmer.org/gears/InvoluteSpurGearBuilder.html?circularPitch=8&pressureAngle=20&clearance=0.05&backlash=0.05&profileShift=0&gear1ToothCount=0&gear1CenterHoleDiamater=0&gear2ToothCount=12&gear2CenterHoleDiamater=1&showOption=3
-%
-% Angle of pressure determines the angle of the rack teeth because they need to be normal to each other.
-% In order for two surfaces to touch at a point, they need to be parallel (or discontinuous).
-% And the pressure is a normal force, QED.
-% 
-% The rack and pinion can both be represented by half a tooth, which is reflected and then transformed.
-% Rotated n times around for the pinion.
-% Translated up to times for the rack.
-%
-% To determine how far each should move, each rotation of one pinion tooth is a translation of one rack tooth.
-% With width calculated as peak to peak for both
-% 360/n deg -> (width of rack tooth, pk2pk)
-%
-% There is a sin(angle of pressure) transverse force.
-%
-% I should add tracks for the handle piece moving backwards, it often gets stuck from slight rotations.
-% I can make this with lines cut out of the outer layer.
-% There needs to be a name for this kind of mechanism.
-
 clear;
 clf;
 more off;
 clc;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% USER EDITABLE SETTINGS
+
+% cm, deg
+
+% circle count, number of points in a calculated arc
+CURVE_SIZE = 30;
+
+% material width
+MAT_W = 2.54 / 4;
+
+% used for handle
+INDEX_FINDER_WIDTH = 2;
+
+BOLT_DIA   = 0.41656 - 0.02; % body     width for bolt % #8 0.1640" / 4.1656 mm minus some kerfing
+HEAD_DIA   = 1.0;            % head/nut width for bolt
+SPACER_DIA = 1.0;            % spacer   width for bolt
+BOLT_RAD   = BOLT_DIA / 2;
+HEAD_RAD   = HEAD_DIA / 2;
+SPACER_RAD = SPACER_DIA / 2;
+
+BAR_COUNT = 5;  % number of bar pairs
+
+% comment out one, it will be calculated instead of defined
+BAR_W = 1.5 * max(HEAD_DIA, SPACER_DIA); % width of each individual bar
+BAR_L = 10; % length of each individual bar
+%THETA = 15.0; % final angle of the bar with the horizon, deg
+
+if exist('BAR_W', 'var') && exist('BAR_L', 'var') && ~exist('THETA', 'var')
+    THETA = asind(2 * BAR_W / BAR_L) / 2;
+elseif exist('BAR_W', 'var') && ~exist('BAR_L', 'var') && exist('THETA', 'var')
+    BAR_L = BAR_W / (sind(2 * THETA) / 2);
+elseif ~exist('BAR_W', 'var') && exist('BAR_L', 'var') && exist('THETA', 'var')
+    BAR_W = BAR_L * sind(2 * THETA) / 2;
+else
+    disp('ERROR: Bars are overdefined.')
+    disp('PICK 2: BAR_L, BAR_W, THETA')
+end
+
+% rope radius, inner radius of the rotor along which the rope is slotted
+PINION_R = cosd(THETA) * BAR_L / 2;
+
+% THETA is the final angle of the bar with the horizon.
+% 0deg would create perfectly flat lines, ideal, but impossible
+% anything 45deg and above makes no sense
+%
+% BAR_W = BAR_L * sind(THETA) * cosd(THETA)
+% BAR_W = BAR_L * sind(2 * THETA) / 2
+% 2 * BAR_W / BAR_L = sind(2 * THETA)
+% arcsind(2 * BAR_W / BAR_L) = 2 * THETA
+% arcsind(2 * BAR_W / BAR_L) / 2 = THETA
+% THETA = asind(2 * BAR_W / BAR_L) / 2
+%
+% note: sin(2 * x) = 2 * sin(x) * cos(x)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% USEFUL CALCULATIONS
+
+% half-length/width
+BAR_HL = BAR_L / 2;
+BAR_HW = BAR_W / 2;
+
+PHI = 90 - THETA;
+OMEGA = PHI - THETA;
+
+THETA_TO_PHI = linspace(THETA, PHI, CURVE_SIZE)';
+
+% separation between points when fully extended (long) and retracted (short)
+REACH_LONG  = BAR_HL * cosd(THETA);
+REACH_SHORT = BAR_HL * sind(THETA);
+
+FRONT_EDGE = (2 * BAR_COUNT + 0.5) * REACH_SHORT;
+BACK_EDGE  = -1 * (0 + 3*BAR_HW);
+
+HNDL_W = MAT_W * 2;
+HNDL_INNER_W = INDEX_FINDER_WIDTH + MAT_W + 0;
+HNDL_OUTER_W = HNDL_W + HNDL_INNER_W;
+HNDL_INNER_H = INDEX_FINDER_WIDTH * 4;
+HNDL_OUTER_H = HNDL_W + HNDL_INNER_H + HNDL_W;
+HNDL_INNER_HH = HNDL_INNER_H / 2;
+HNDL_OUTER_HH = HNDL_OUTER_H / 2;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GENERIC CIRCLE (FOR EASY PLOTTING)
+
+% CCW
+%      _---|<--_
+%     /    |    \
+%    V   2 | 1   |
+%   -------+-------
+%    |   3 | 4   A
+%     \_   |   _/
+%       -->|---
+%
+% CCW 1: [ c,  s]
+% CCW 2: [-s,  c]
+% CCW 3: [-c, -s]
+% CCW 4: [ s, -c]
+%
+% CCW
+%      _-->|---_
+%     /    |    \
+%    |   2 | 1   V
+%   -------+-------
+%    A   3 | 4   |
+%     \_   |   _/
+%       ---|<--
+%
+% CCW 1: [ s,  c]
+% CCW 2: [-c,  s]
+% CCW 3: [-s, -c]
+% CCW 4: [ c, -s]
+%
+UNIT_CIRCLE = linspace(0, 90, CURVE_SIZE)';
+COS_ARC = cosd(UNIT_CIRCLE);
+SIN_ARC = flipud(COS_ARC);
+UNIT_CIRCLE = [ COS_ARC,  SIN_ARC;
+               -SIN_ARC,  COS_ARC;
+               -COS_ARC, -SIN_ARC;
+                SIN_ARC, -COS_ARC;
+              ];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% BARS
+
+LINE = [0, -BAR_HL;
+        0,  0;
+        0, +BAR_HL];
+
+%                    |---------------| BAR_HW
+%
+% /                  +---------------+---------------+  -         -
+%  \                 |\\             |               |  |         |
+%   \                | \ \           |               |  |         |
+%    \               |  \  \         |               |  |         |
+%     \              |   \   \       |               |  | BAR_LE  |
+%      \             |    \    \     |               |  |         |
+%       \            |     \     \   |               |  |         |
+%        \           |      \      \ | hole here     |  |         |
+%         \          +-------\-------+---------------+  -         | BAR_SR
+%  BAR_CR  \         |        \      |               |            |
+%           \        |         \     |               |            |
+%            \       |          \    |               |            |
+%             \      |           \   |               |            |
+%              \     |            \  |               |            |
+%               \    |             \ |               |            |
+%                \   |              \|               |            |
+%                 /  |               +---------------+  -         -
+%                    |               ^               |  |
+%                    |       curve centers here      |  |
+%                    |                               |  |
+%                    |                               |  | BAR_CS
+%                    |                               |  |
+%                    |                               |  |
+%                    |          center hole          |  |
+%                    |               +               |  -
+%                    |                               |
+%                    .           other side          .
+%                    .              |                .
+%                    .              v                .
+%
+% BAR_L  : bar length from end-hole to end-hole
+% BAR_W  : bar width from side to side
+% BAR_HL : half-length
+% BAR_HW : half-width
+%
+% BAR_LE : Length exstension necessary for the outside edges to reathe the next bar's outside edge when extended
+% BAR_CR : Radius of the cruve at the end of the bar. 
+%          The start fo the curve is matched to that angle at which the bars meet to hide it.
+% BAR_SR : Sub-radial distance, subtract this before adding the curve to account for its long radius.
+% BAR_CS : Length from middle of bar to curve center
+%
+% BAR_HL only reaches from the center to the hole.
+% Need to provide more area for the screw and make the edges form a continuous line.
+% By extending the bar by BAR_LE, and making and arc over the end, we can create more area.
+% The arc should be of raduis BAR_CR so that it's angle matches the outline of the next bar.
+% And by using a circular curve instead of a pointy end, it'll be smaller (vertically) when retracted.
+% And less dangerous.
+%
+BAR_LE = BAR_HW * tand(THETA); % difference from where the front/back hole to where the curve starts
+BAR_SR = BAR_HW * tand(2 * THETA);
+BAR_CR = BAR_HW * secd(2 * THETA);
+BAR_CS = BAR_HL + BAR_LE - BAR_SR;
+BAR_R = BAR_CS + BAR_CR; % bar max radius
+
+INNER_HEIGHT = BAR_R * cosd(THETA); % height (from center) of bar while inside
+OUTER_HEIGHT = INNER_HEIGHT + BAR_HW;
+OUTER_RADIUS = sqrt(OUTER_HEIGHT^2 + (4*MAT_W)^2);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% REGULAR BARS
+
+OMEGA_RANGE = linspace(-OMEGA, +OMEGA, CURVE_SIZE)';
+
+% create curves at end
+POINTS = [0, BAR_CS] + BAR_CR * [sind(OMEGA_RANGE), cosd(OMEGA_RANGE)];
+% duplicate and rotate 180deg
+POINTS = [POINTS; -1*POINTS];
+% return to start
+POINTS = [POINTS;POINTS(1,:)];
+
+% add holes to bars
+BAR_POINTS = [
+              POINTS;
+              NaN, NaN;
+              UNIT_CIRCLE * BOLT_RAD + [0,-BAR_HL]; % hole at back
+              NaN, NaN;
+              UNIT_CIRCLE * BOLT_RAD + [0,0]; % hole in middle
+              NaN, NaN;
+              UNIT_CIRCLE * BOLT_RAD + [0,BAR_HL]; % hole at front
+              ];
+
+COVER_POINTS = [
+                POINTS;
+                NaN, NaN;
+                UNIT_CIRCLE * HEAD_RAD + [0,-BAR_HL]; % hole at back
+                NaN, NaN;
+                UNIT_CIRCLE * HEAD_RAD + [0,0]; % hole in middle
+                NaN, NaN;
+                UNIT_CIRCLE * HEAD_RAD + [0,BAR_HL]; % hole at front
+                ];
+
+clear POINTS;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% START BARS
+
+% final bar (half-length)
+POINTS = [
+          BAR_CR * -sind(OMEGA_RANGE), BAR_CS + BAR_CR * cosd(OMEGA_RANGE); % back
+          BAR_HW * -COS_ARC, BAR_HW * -SIN_ARC;
+          BAR_HW *  SIN_ARC, BAR_HW * -COS_ARC;
+          ];
+% conect end to start
+POINTS = [
+          POINTS;
+          POINTS(1,:);
+          ];
+% add holes to bars
+START_BAR_POINTS = [
+                    POINTS;
+                    NaN, NaN;
+                    (UNIT_CIRCLE * BOLT_RAD + [0, 0]); % hole at back
+                    NaN, NaN;
+                    (UNIT_CIRCLE * BOLT_RAD + [0,+BAR_HL]); % hole in middle
+                    ];
+
+START_COVER_POINTS = [
+                      POINTS;
+                      NaN, NaN;
+                      UNIT_CIRCLE * HEAD_RAD + [0, 0]; % hole at back
+                      NaN, NaN;
+                      UNIT_CIRCLE * HEAD_RAD + [0,+BAR_HL]; % hole in middle
+                      ];
+
+clear POINTS;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% END BARS
+
+PSI = 45;
+CORNER_OFFSET = BAR_HW * cot(PSI);
+
+% final bar (half-length)
+POINTS = [
+          BAR_HL * cosd(PSI) - BAR_HW * cosd(PSI - 90), - BAR_HL * sind(PSI) + BAR_HW * sind(PSI - 90);
+          BAR_HL * cosd(PSI) + BAR_HW * cosd(PSI - 90), - BAR_HL * sind(PSI) - BAR_HW * sind(PSI - 90);
+          BAR_HW, CORNER_OFFSET;
+          BAR_CR * -sind(OMEGA_RANGE), BAR_CS + BAR_CR * cosd(OMEGA_RANGE) % back
+          ];
+[a1, ~] = getTangentialLineToCircle(POINTS(1,:), [0 0], BAR_HW);
+[~, a2] = getTangentialLineToCircle(POINTS(end,:), [0 0], BAR_HW);
+A_RANGE = linspace(a2, a1, CURVE_SIZE)';
+POINTS = [
+          POINTS;
+          BAR_HW * [cosd(A_RANGE), sind(A_RANGE)];
+          ];
+% conect end to start
+POINTS = [
+          POINTS;
+          POINTS(1,:);
+          ];
+% flip vertically
+POINTS = POINTS * [1,0;0,-1];
+% add holes to bars
+END_BAR_POINTS = [
+                  POINTS;
+                  NaN, NaN;
+                  UNIT_CIRCLE * BOLT_RAD; % hole at back
+                  NaN, NaN;
+                  UNIT_CIRCLE * BOLT_RAD + [0,-BAR_HL]; % hole in middle
+                  ];
+
+END_COVER_POINTS = [
+                    POINTS;
+                    NaN, NaN;
+                    UNIT_CIRCLE * HEAD_RAD; % hole at back
+                    NaN, NaN;
+                    UNIT_CIRCLE * HEAD_RAD + [0,-BAR_HL]; % hole in middle
+                    ];
+
+clear PSI CORNER_OFFSET a1 a2 A_RANGE POINTS;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% RACK & PINION
+
 
 % pinion (half-tooth)
 % 12 teeth (each rotated 360/12 = 30deg = pi/6)
 % rotate around 0
 % reflect across start
 P_pt = [
-        0,       -12.6824;
-        0.4307,  -12.6751;
-        0.9341,  -12.6480;
-        0.9244,  -12.6488;
-        0.9476,  -12.6472;
-        0.9613,  -12.6470;
-        0.9479,  -12.6478;
-        0.9749,  -12.6472;
-        1.0843,  -12.6669;
-        1.1927,  -12.7180;
-        1.2976,  -12.8003;
-        1.3966,  -12.9136;
-        1.4873,  -13.0576;
-        1.5672,  -13.2316;
-        1.6340,  -13.4351;
-        1.6855,  -13.6670;
-        1.7194,  -13.9266;
-        1.7334,  -14.2126;
-        1.7330,  -14.4206;
-        1.7775,  -14.5860;
-        1.8684,  -14.8388;
-        2.0235,  -15.1784;
-        2.1288,  -15.3759;
-        2.2547,  -15.5900;
-        2.4039,  -15.8212;
-        2.5770,  -16.0661;
-        2.7765,  -16.3247;
-        3.0017,  -16.5925;
-        3.2556,  -16.8702;
-        3.5394,  -17.1554;
-        3.8162,  -17.4120;
+        -0.18363, -0.68532;
+        -0.16024, -0.69117;
+        -0.13265, -0.69699;
+        -0.13320, -0.69690;
+        -0.13192, -0.69715;
+        -0.13117, -0.69733;
+        -0.13190, -0.69718;
+        -0.13044, -0.69754;
+        -0.12481, -0.70019;
+        -0.11970, -0.70452;
+        -0.11522, -0.71048;
+        -0.11151, -0.71804;
+        -0.10869, -0.72714;
+        -0.10689, -0.73770;
+        -0.10623, -0.74965;
+        -0.10681, -0.76294;
+        -0.10873, -0.77745;
+        -0.11212, -0.79311;
+        -0.11515, -0.80435;
+        -0.11514, -0.81393;
+        -0.11388, -0.82890;
+        -0.11043, -0.84950;
+        -0.10760, -0.86170;
+        -0.10389, -0.87509;
+        -0.09918, -0.88974;
+        -0.09336, -0.90548;
+        -0.08633, -0.92235;
+        -0.07804, -0.94008;
+        -0.06834, -0.95876;
+        -0.05713, -0.97828;
+        -0.04590, -0.99616;
+        -0.00000, -1.00000;
         ];
+P_pt = P_pt * PINION_R;
 n = 12;
-P_t = [flipud(reflectAcrossLine(P_pt, P_pt(1,2)/P_pt(1,1), 0)); P_pt];
-P_pinion = [];
-for i = 1:n
-    P_pinion = [P_pinion; P_t * rotccwd(-i*360/n)];
-end
-P_pinion = [P_pinion; P_pinion(1,:)];
+psi = 360 / n;
+P_t = [P_pt; flipud(reflectAcrossLine(P_pt, Inf, 0))];
 
+P_pinion = [];
+for ii = -1:ceil(OMEGA/psi)
+    P_pinion = [P_pinion; P_t * rotccwd(-ii*360/n)];
+end
+ROUND_RADIUS = norm(P_pinion(1,:), 2);
+t1 = atan2d(P_pinion(end,2), P_pinion(end,1));
+t2 = atan2d(P_pinion(1,2), P_pinion(1,1)) + 360;
+ROUND_RANGE = linspace(t1, t2, CURVE_SIZE)';
+
+[~, a1] = getTangentialLineToCircle(P_pinion(end,:), [0 0], BAR_HW);
+[a2, ~] = getTangentialLineToCircle(P_pinion(1,:),   [0 0], BAR_HW);
+% add 2*theta to get the junction to be flat
+A_RANGE = linspace(a1, a2, CURVE_SIZE)'                + 2 * THETA;
+
+P_pinion = [
+            P_pinion;
+            BAR_HW * [cosd(A_RANGE), sind(A_RANGE)];
+            P_pinion(1,:);
+            ];
+
+P_pinion = [
+            P_pinion
+            NaN, NaN;
+            UNIT_CIRCLE * BOLT_RAD;
+            ];
 
 % rack teeth
 P_rt = [
-        0.00000,  2.54648;
-        1.11575,  2.54648;
-        2.87475, -2.59648;
-        4.00000, -2.59648;
-        ]
-P_rt = P_rt + [0, -15.278874397277832];
-w = 8.0
-P_r = [flipud(reflectAcrossLine(P_rt, Inf, 0)); P_rt];
+        -0.22378, -0.71230;
+        -0.16136, -0.71230;
+        -0.06295, -1.00000;
+        -0.00000, -1.00000;
+        ];
+P_rt = P_rt * PINION_R;
+w = (P_rt(end,1) - P_rt(1,1)) * 2;
+P_r = [P_rt; flipud(reflectAcrossLine(P_rt, Inf, 0))];
+
 P_rack = [];
-n
-for i = 1:floor(n/4)
-    i
-    P_rack = [P_rack; (P_r + [(i-1)*w, 0])];
-    size(P_rack)
+for ii = -1:ceil(OMEGA/psi)
+    P_rack = [P_rack; (P_r + [ii*w, 0])];
 end
 
-hold on
-plot(P_pinion(:,1), P_pinion(:,2))
-plot(P_rack(:,1), P_rack(:,2))
-axis('equal')
-hold off
+RACK_UNIT_PER_DEG = w / psi;
+
+clear n w P_pt P_p P_rt P_r;
+% P_pinion
+% P_rack
+% RACK_UNIT_PER_DEG
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SIMPLIFY PLOTTING
+
+c1 = size(START_BAR_POINTS,1);
+c2 = size(BAR_POINTS,1);
+c3 = size(END_BAR_POINTS,1);
+L1 = NaN*ones(c1 + c2*(BAR_COUNT-1) + c3 + BAR_COUNT,3);
+L2 = NaN*ones(c1 + c2*(BAR_COUNT-1) + c3 + BAR_COUNT,3);
+L1(1:c1,:) = [START_BAR_POINTS 0*ones(c1,1)];
+L2(1:c1,:) = [START_COVER_POINTS 0*ones(c1,1)];
+for ii = 1:BAR_COUNT-1
+    indx = (c1 + 1) + (ii-1) * (c2 + 1);
+    L1(indx+1:(indx+c2),:) = [BAR_POINTS ii*ones(c2,1)];
+    L2(indx+1:(indx+c2),:) = [COVER_POINTS ii*ones(c2,1)];
+end
+indx = (c1 + 1) + (BAR_COUNT-1) * (c2 + 1);
+L1((indx+1):(indx+c3),:) = [END_BAR_POINTS BAR_COUNT*ones(c3,1)];
+L2((indx+1):(indx+c3),:) = [END_COVER_POINTS BAR_COUNT*ones(c3,1)];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PLOT ANIMATION
+
+if true
+    figure(1);
+    clf;
+    hold on;
+    jjmax = 2;
+    
+    for jj = 0:jjmax
+        % get angle and separation distances
+        completion = jj / jjmax;
+        ZETA = THETA + OMEGA * completion;
+        PULL = OMEGA * completion * RACK_UNIT_PER_DEG;
+        
+        % draw above previous
+        dy = (jj-1) * BAR_L * 2;
+        % bar separation at this angle
+        dx = BAR_L * sind(ZETA);
+        DX = [dx 0];
+        
+        % rotate bars
+        R = rotccwd(-ZETA);
+        NP = (rotccwd(-OMEGA * completion) * (P_pinion)')';
+        NB = (R * (L1(:,1:2))')' + L1(:,3)*DX;
+        NC = (R * (L2(:,1:2))')' + L2(:,3)*DX;
+        NR = P_rack + [-PULL, 0];
+        
+        plot(NR(:,1),              dy + NR(:,2),           'k');
+        plot(NP(:,1),              dy + NP(:,2),           'k');
+        plot(NC(:,1),              dy + NC(:,2),           'r');
+        plot(NB(:,1),              dy + NB(:,2),           'b');
+
+        %plot(NB(:,1),              dy - NB(:,2),           'b');
+        %plot(NC(:,1),              dy - NC(:,2),           'r');
+        %plot(NP(:,1),              dy - NP(:,2),           'k');
+        %plot(NR(:,1),              dy - NR(:,2),           'k');
+
+        % fixed origin point
+        plot(0,dy,'r+');
+    end
+    axis('equal');
+    axis('off');
+    hold off;
+end
