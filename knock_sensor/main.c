6fae31ecafd780264219d370f26458df84bafb31
gercunderscore4 <gercunderscore4@gmail.com>
2024-06-13 00:31:59 -0400

    got tools working, can now compile and flash from Windows

--- knock_sensor/main.c
+++ knock_sensor/main.c
@@ -9,16 +9,16 @@
  * PA6 -+----+- PA5
  *
  * PA0 : trigger
- * PA1 : 
- * PA2 : 
- * PA3 : 
- * PA5 : 
- * PA6 : 
+ * PA1 :
+ * PA2 :
+ * PA3 :
+ * PA5 :
+ * PA6 :
  * PA7 : PWM servo
  *
  * Useful registers
  * ----------------
- * 
+ *
  * DDRB  - Port B Data Direction Register
  * 0 = INPUT, 1 = OUTPUT
  *
@@ -32,140 +32,22 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "stdlib.h"
-#include "InterruptAudio.h"
-#include "AnalogInput.h"
-#include "ArduinoLike.h"
 #include "pwm.h"
 
 /****************************************************************
  * MAIN
  ****************************************************************/
 
-/*
-int main (void) {
-    init_audio();
-
-    toneOn(0);
-
-    uint16_t timeBlock = 0;
-    uint16_t f = 0;
-    uint16_t t = 0;
-    while (1) {
-        // these are manually picked through testing
-        f = randomWMinMax(6000, 7000);
-        t = randomWMinMax(2, 4);
-        timeBlock += t;
-        changeTone(f);
-        delay(t);
-    }
-    while(1) {
-        for (f = 1000; f <= 20000; f += 1000) {
-            changeTone(f);
-            delay(1000);
-        }
-    }
-
-    noTone();
-
-    return 1;
-}
-*/
-
-//      ANALOG_PIN   // PA0
-#define FUSE_1_PIN 1 // PA1
-#define FUSE_2_PIN 2 // PA2
-#define FUSE_3_PIN 3 // PA3
-#define MOTOR_PIN  5 // PA5
-//      AUDIO_PIN    // PA6
-//      FLASH_PIN    // PA7
-
-uint8_t debounceTrigger (void) {
-    if (analogRead() > TRIGGER_THRESH) {
-        delay(50);
-        if (analogRead() > TRIGGER_THRESH) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-void waitForTrigger (void) {
-    while (!debounceTrigger());
-}
-
 void setup (void) {
     pwmInit();
-
-    // set outputs
-    pinMode(FUSE_1_PIN, OUTPUT);
-    pinMode(FUSE_2_PIN, OUTPUT);
-    pinMode(FUSE_3_PIN, OUTPUT);
-    pinMode(MOTOR_PIN,  OUTPUT);
-    
-    // set all low
-    digitalWrite(FUSE_1_PIN, LOW);
-    digitalWrite(FUSE_2_PIN, LOW);
-    digitalWrite(FUSE_3_PIN, LOW);
-    digitalWrite(MOTOR_PIN,  LOW);
 }
 
 void loop (void) {
-    // wait for trigger to start
-    waitForTrigger();
-    
-    // fuse
-    toneOn(0);
-    sparkleFuse(FUSE_1_PIN);
-    sparkleFuse(FUSE_2_PIN);
-    sparkleFuse(FUSE_3_PIN);
-    noTone();
-
-    // if user is still holding the fuse, don't explode
-    if (debounceTrigger()) {
-        delay(3000);
-        return;
-    }
-    
-    // start motor
-    digitalWrite(MOTOR_PIN, HIGH);
-
-    int fMax = 0;
-    int timeBlock = 0;
-    int time_ms = 0;
-    int f = 0;
-    int t = 0;
-    // decrease f and brightness to reach zero together
-    fMax = 1600;
-    time_ms = 5;
-    uint8_t brightness = 0xFF;
-    while (fMax) {
-        // set brightness, then divide by 2
-        pwmWrite(brightness);
-        brightness >>= 1;
-
-        // play 100ms of sound
-        // and decrease f
-        timeBlock = 0;
-        while (timeBlock < time_ms) {
-            f = randomWMinMax(0, fMax);
-            t = randomWMinMax(0,    3);
-            timeBlock += t;
-            toneOn(f);
-            delay(t);
-        }
-        fMax -= 100;
-        fMax = max(0, fMax);
-        time_ms += 5;
-    }
-    noTone();
+    pwmWrite(0x00);
+    delay(2000);
+    pwmWrite(0x80);
+    delay(3000);
     pwmOff();
-
-    // stop motor
-    digitalWrite(MOTOR_PIN, LOW);
-    digitalWrite(MOTOR_PIN, HIGH);
-    digitalWrite(MOTOR_PIN, LOW);
-
-    return;
 }
 
 int main (void) {
